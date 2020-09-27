59c34d1012eb0744c2e55ee4f26b31323a7efcf3
Caleb <calebdiaza12@icloud.com>
2020-09-26 19:29:43 -0700

    cree el proyecto

---
+++ Post Reader/Post Reader/Post_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Post_ReaderApp.swift
+//  Post Reader
+//
+//  Created by Caleb Haziel Diaz Antonio on 9/26/20.
+//
+
+import SwiftUI
+
+@main
+struct Post_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
